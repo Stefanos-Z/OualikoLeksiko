8ebcfcc784bf2bb504668b165f8d9e8ae3c435dc
oneZt <oneZt@MSI>
2018-04-19 14:46:06 +0100

    Merge origin/master
    Conflicts:
    	src/main/webapp/WEB-INF/web.xml

--- src/main/java/com/mycompany/welsh_tests/EditWordsServlet.java
+++ src/main/java/com/mycompany/welsh_tests/EditWordsServlet.java
@@ -7,10 +7,12 @@ package com.mycompany.welsh_tests;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.WelshWord;
 
 /**
  *
@@ -18,31 +20,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class EditWordsServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet EditWordsServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet EditWordsServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
+
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
@@ -56,7 +34,52 @@ public class EditWordsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        DatbaseInterface inter = new DatbaseInterface();
+        inter.getConection();
+        ArrayList<WelshWord> allWords = inter.getWelshWords();
+        response.setContentType("text/html;charset=UTF-8");
+        
+        
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"/>");
+        out.println("<link rel=\"icon\" href=\"images/add_remove_words.png\"/>");
+        out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/menu_and_background.css\"/>");
+        out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/tables.css\"/>");
+        out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/wordsManager.css\"/>");
+        out.println("<script src=\"js/displayMenuButtons.js\"></script>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<table class=\"wordsTable\">");
+        out.println("<tr>");
+                out.println("<th align=\"center\" class=\"columnLabel\">");
+                    out.println("Welsh Language <img class=\"columnDataImage\" src=\"images/welsh-flag.png\"/>");
+                out.println("</th>");
+                out.println("<th class=\"columnLabel\">");
+                    out.println("English Language <img class=\"columnDataImage\" src=\"images/uk-flag.png\"/></th>");
+                out.println("<th class=\"columnLabel\">");
+                    out.println("Gender of the word <img class=\"columnDataImage\" src=\"images/gender.png\"/></th>");
+                out.println("<th class=\"columnLabel\">");
+                    out.println("Options<img class=\"columnDataImage\" src=\"images/option.png\"/></th>");
+            out.println("</tr>");
+        for(int i =0; i<allWords.size();i++){
+                out.println("<tr class=\"columnRow\">");
+                    out.println("<td id=\"columnData\">"+allWords.get(i).getWelshWord()+"</td>");
+                    out.println("<td id=\"columnData\">"+allWords.get(i).getEnglishMeaning()+"</td>");
+                    out.println("<td id=\"columnData\">"+allWords.get(i).getGender()+"</td>");
+                    out.println("<td id=\"columnData\">");
+                        out.println("<img class=\"editWordImage\" src=\"images/editWord.png\"/>");
+                        out.println("<img class=\"deleteWordImage\" src=\"images/deleteWord.png\"/>");
+                    out.println("</td>");
+                out.println("</tr>");
+
+        }
+        out.println("</table>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+        
     }
 
     /**
@@ -70,7 +93,6 @@ public class EditWordsServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
     }
 
     /**
