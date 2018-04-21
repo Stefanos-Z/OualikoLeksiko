578ae3d37ed934fa6bbc5f588bfb57d0977baf2c
Loizos V. <eeu905@bangor.ac.uk>
2018-04-21 02:11:06 +0100

    Na sas miliso pou konta eshi polles doulies na kamoume :P Allaxa kamposa pramata ;)

--- src/main/webapp/js/displayHomePageElements.js
+++ src/main/webapp/js/displayHomePageElements.js
@@ -1,43 +1,57 @@
-//REFERENCE : https://support.zendesk.com/hc/en-us/articles/203661316-Hide-or-show-HTML-based-on-user-s-role-or-group
-//
-//Show div html based on role
 
+/* Variables Declaration */
 var currentType = "ALL";
 
-if(currentType === "Student"){
-    document.getElementById("userType_HOME").style.display = 'block';
-    document.getElementById("userType_STUDENT").style.display = 'block';
-    document.getElementById("userType_HISTORY").style.display = 'block';
-    document.getElementById("userType_LOGOUT").style.display = 'block';
-    
-    document.getElementById("menuInstructor").style.display = 'none';
-    document.getElementById("menuAdministrator").style.display = 'none';
 
-}
-else if(currentType === "Instructor"){
-    document.getElementById("userType_HOME").style.display = 'block';
-    document.getElementById("userType_INSTRUCTOR").style.display = 'block';
-    document.getElementById("userType_HISTORY").style.display = 'block';
-    document.getElementById("userType_LOGOUT").style.display = 'block';
+displayHomePageElements(currentType);
+
+
+function displayHomePageElements(currentType){
     
-    document.getElementById("menuStudent").style.display = 'none';
-    document.getElementById("menuAdministrator").style.display = 'none';
-}
-else if(currentType === "Administrator"){
-    document.getElementById("userType_HOME").style.display = 'block';
-    document.getElementById("userType_HISTORY").style.display = 'block';
-    document.getElementById("userType_ADMINISTRATOR").style.display = 'block';
-    document.getElementById("userType_LOGOUT").style.display = 'block';    
-    
-    document.getElementById("menuStudent").style.display = 'none';
-    document.getElementById("menuInstructor").style.display = 'none';
-}
-else if(currentType === "ALL" || currentType === ""){
-    document.getElementById("userType_HOME").style.display = 'block';
-    document.getElementById("userType_INSTRUCTOR").style.display = 'block';
-    document.getElementById("userType_STUDENT").style.display = 'block';
-    document.getElementById("userType_HISTORY").style.display = 'block';
-    document.getElementById("userType_ADMINISTRATOR").style.display = 'block';
-    document.getElementById("userType_LOGOUT").style.display = 'block';
-    
+    if(currentType === "Student"){
+        /* Display Elements in Menu Bar */
+        document.getElementById("userType_HOME").style.display = 'block';
+        document.getElementById("userType_STUDENT").style.display = 'block';
+        document.getElementById("userType_HISTORY").style.display = 'block';
+        document.getElementById("userType_LOGOUT").style.display = 'block';
+
+        /* Display Main Elements in the middle of the Screen */
+        document.getElementById("menuInstructor").style.display = 'none';
+        document.getElementById("menuAdministrator").style.display = 'none';
+
+    }
+    else if(currentType === "Instructor"){
+        /* Display Elements in Menu Bar */
+        document.getElementById("userType_HOME").style.display = 'block';
+        document.getElementById("userType_INSTRUCTOR").style.display = 'block';
+        document.getElementById("userType_HISTORY").style.display = 'block';
+        document.getElementById("userType_LOGOUT").style.display = 'block';
+
+        /* Display Main Elements in the middle of the Screen */
+        document.getElementById("menuStudent").style.display = 'none';
+        document.getElementById("menuAdministrator").style.display = 'none';
+    }
+    else if(currentType === "Administrator"){
+        /* Display Elements in Menu Bar */
+        document.getElementById("userType_HOME").style.display = 'block';
+        document.getElementById("userType_HISTORY").style.display = 'block';
+        document.getElementById("userType_ADMINISTRATOR").style.display = 'block';
+        document.getElementById("userType_LOGOUT").style.display = 'block';    
+
+        /* Display Main Elements in the middle of the Screen */
+        document.getElementById("menuStudent").style.display = 'none';
+        document.getElementById("menuInstructor").style.display = 'none';
+    }
+    else if(currentType === "ALL" || currentType === ""){
+        /* Display Elements in Menu Bar */
+        document.getElementById("userType_HOME").style.display = 'block';
+        document.getElementById("userType_INSTRUCTOR").style.display = 'block';
+        document.getElementById("userType_STUDENT").style.display = 'block';
+        document.getElementById("userType_HISTORY").style.display = 'block';
+        document.getElementById("userType_ADMINISTRATOR").style.display = 'block';
+        document.getElementById("userType_LOGOUT").style.display = 'block';
+        
+        /* Display Main Elements in the middle of the Screen */
+        //Display All - Dont take action.
+    }
 }
