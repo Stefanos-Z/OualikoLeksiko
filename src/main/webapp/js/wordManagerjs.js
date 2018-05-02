f220e85fd597e7991b1bd20eb9e091fd98b4559d
oneZt <oneZt@147.143.129.49>
2018-05-02 15:45:52 +0100

    Merge origin/master
    Conflicts:
    	src/main/java/com/mycompany/welsh_tests/WordsManagerServlet.java

--- src/main/webapp/js/wordManagerjs.js
+++ src/main/webapp/js/wordManagerjs.js
@@ -6,4 +6,27 @@
 
 $(document).ready(function(){
     console.log('hello');
+    
+    
+    $("#AddWordToDB").click(function() {
+        var wWord = $("#wordWelshAdd").val();
+        console.log(wWord);
+        
+        var data = {};
+        $.ajax({
+            type : "POST",
+            contentType : "application/x-www-form-urlencoded",
+            url : "/Welsh_test/AddWordServlet",
+            data : word,
+            dataType : 'text',
+            success : function(response) {
+                console.log("Success!");
+            },
+            error : function(e) {
+                    alert("ERROR");
+                    
+            }
+        });
+        
+    });
 });
